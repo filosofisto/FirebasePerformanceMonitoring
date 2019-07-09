2e114074b5358aebc45e08b172ff7f3773d8b38f
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2019-07-09 13:54:19 -0300

    Initial Commit

---
+++ FirebasePerformanceMonitoring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebasePerformanceMonitoring
+//
+//  Created by Eduardo Ribeiro da Silva on 09/07/19.
+//  Copyright © 2019 Eduardo Ribeiro da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
